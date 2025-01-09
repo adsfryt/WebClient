6c3473b5db1cfb37b2acd0ff41117a35d715f100
adsfryt <93540011+adsfryt@users.noreply.github.com>
2025-01-10 02:38:09 +0300

    Update Data.js

--- Data.js
+++ Data.js
@@ -4,10 +4,10 @@ class Data{
     constructor() {
         makeAutoObservable(this)
     }
-    ADDRESS_SITE="http://localhost:3000"
-    ADDRESS_SERVER="http://localhost:5000"
-    Auth_Address="http://localhost:6500"
-    WS_ADDRESS_SERVER="ws://localhost:5000"
+    ADDRESS_SITE="http://79.174.91.102:3000"
+    ADDRESS_SERVER="http://79.174.91.102:5000"
+    Auth_Address="http://79.174.91.102:6500"
+    WS_ADDRESS_SERVER="ws://79.174.91.102:5000"
 }
 
 export default new Data()
